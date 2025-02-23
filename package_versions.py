d6ae84c720668604dad81993c41c3ab99e144796
stillmattwest <stillmattwest@gmail.com>
2025-02-23 09:59:54 -0800

    initial commit

---
+++ package_versions.py
@@ -0,0 +1,11 @@
+#inputs = [1.2, 51., 2.1] # these are inputs coming from the previous layer in a NN
+
+import sys
+import numpy as np
+import matplotlib
+
+print("Python:", sys.version)
+print("Numpy:", np.__version__)
+print("Matplotlib:", matplotlib.__version__)
+
+
