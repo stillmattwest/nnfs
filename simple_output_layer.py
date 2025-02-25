a9a8f4f7aa82687828daede725bc6075c432299a
stillmattwest <stillmattwest@gmail.com>
2025-02-24 19:01:56 -0800

    refactor output layer calculations and improve function naming

--- simple_output_layer.py
+++ simple_output_layer.py
@@ -2,30 +2,25 @@
 
 # in this example we have three neurons in our output layer, and four in the previous layer. That means each output neuron needs four inputs, four weights, and one bias.
 
-inputs = [1,2,3,2.5]
-inputs2 = [0.2,0.8,-0.5,1.0]
-inputs3 = [-0.26,-0.27,0.17,0.87]
-
-weights1 = [0.2,0.8,-0.5]
-weights2 = [0.2,0.8,-0.5,1.0]
+inputs = [1,2,3,2.5] # the outputs of the four neurons in the previous layer
+weights1 = [0.2,0.8,-0.5,1.0] # the weights connecting the previous layer to the first output neuron
+weights2=[0.5,-0.91,0.26,-0.5]
 weights3 = [-0.26,-0.27,0.17,0.87]
+weights = [weights1,weights2,weights3] # the weights of the three output neurons
+biases = [2,3,0.5] # the biases of the three output neurons
 
-bias1 = 2
-bias2= 3
-bias3 = 0.5
+outputs = [0.0,0.0,0.0] # the outputs of the three neurons in the output layer
 
-sum1 = 0
-sum2 = 0
-sum3 = 0
-sum4 = 0
-
-def get_sum(inputs:list[float],weights:list[float],bias:float):
+def get_output(inputs:list[float],weights:list[float],bias:float):
     sum = 0
     for index,input in enumerate(inputs):
         sum += input * weights[index]
+    output = sum + bias
+    return output
 
+for output in outputs:
+    for index,weight in enumerate(weights):
+        outputs[index] = get_output(inputs,weight,biases[index])
+    
 
-
-output = sum + bias
-
-print("output:",output) #2.3
+print("outputs:",outputs) #[4.8,1.21,2.385]
