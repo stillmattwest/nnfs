82c8e88dc4cccb20e2890c245e85a82fdc516eaa
stillmattwest <stillmattwest@gmail.com>
2025-03-01 13:41:51 -0800

    refactoring neuron code

--- simple_output_layer.py
+++ simple_output_layer.py
@@ -2,25 +2,17 @@
 
 # in this example we have three neurons in our output layer, and four in the previous layer. That means each output neuron needs four inputs, four weights, and one bias.
 
-inputs = [1,2,3,2.5] # the outputs of the four neurons in the previous layer
-weights1 = [0.2,0.8,-0.5,1.0] # the weights connecting the previous layer to the first output neuron
-weights2=[0.5,-0.91,0.26,-0.5]
-weights3 = [-0.26,-0.27,0.17,0.87]
-weights = [weights1,weights2,weights3] # the weights of the three output neurons
-biases = [2,3,0.5] # the biases of the three output neurons
+inputs = [[1,2,3,2.5],[2.0,5.0,-1.0,2.0],[-1.5,2.7,3.3,-0.8]] # the outputs of the four neurons in the previous layer, now with batching! Each batch is a single input to the network
+weights = [
+    [0.2, 0.8, -0.5, 1.0],  # weights connecting the previous layer to the first output neuron
+    [0.5, -0.91, 0.26, -0.5],
+    [-0.26, -0.27, 0.17, 0.87]
+] # the weights of the three output neurons
+biases = [2, 3, 0.5] # the biases of the three output neurons
 
-outputs = [0.0,0.0,0.0] # the outputs of the three neurons in the output layer
+output = np.dot(inputs, np.array(weights).T) + biases
+print("output:", output) # [[ 4.8   1.21  2.385]]
 
-def get_output(inputs:list[float],weights:list[float],bias:float):
-    sum = 0
-    for index,input in enumerate(inputs):
-        sum += input * weights[index]
-    output = sum + bias
-    return output
 
-for output in outputs:
-    for index,weight in enumerate(weights):
-        outputs[index] = get_output(inputs,weight,biases[index])
-    
 
-print("outputs:",outputs) #[4.8,1.21,2.385]
+print("outputs:", outputs) # [4.8, 1.21, 2.385]
