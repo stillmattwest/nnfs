b07cbbb9f95aa95f4b4d404c5d728475163e0645
stillmattwest <stillmattwest@gmail.com>
2025-04-02 05:37:21 -0700

    tweaking no-calc

--- no-calc-ml.py
+++ no-calc-ml.py
@@ -13,13 +13,13 @@ nnfs.init()
 # create data
 X, y = vertical_data(samples=100, classes=3)
 
-print(X[:10])
-print(y[:10])
+# print(X[:10])
+# print(y[:10])
 
 # create model
-dense1 = Layer_Dense(2, 3)  # first dense layer, 2 inputs
+dense1 = Layer_Dense(2, 6)  # first dense layer, 2 inputs
 activation1 = Activation_ReLU()  # activation function
-dense2 = Layer_Dense(3, 3)  # second dense layer, 3 inputs, 3 outputs
+dense2 = Layer_Dense(6, 3)  # second dense layer, 3 inputs, 3 outputs
 activation2 = Activation_Softmax()  # activation function
 
 # create loss function
@@ -32,38 +32,46 @@ best_dense1_biases = dense1.biases.copy()
 best_dense2_weights = dense2.weights.copy()
 best_dense2_biases = dense2.biases.copy()
 
-# for iteration in range(10000):
-#     # Update weights with some small random values
-#     dense1.weights += 0.05 * np.random.randn(2, 3)
-#     dense1.biases += 0.05 * np.random.randn(3)
-#     dense2.weights += 0.05 * np.random.randn(3, 3)
-#     dense2.biases += 0.05 * np.random.randn(3)
+for iteration in range(10000):
+    # Update weights with some small random values
+    dense1.weights += np.random.uniform(
+        -0.05, 0.05, size=dense1.weights.shape
+    ) * np.random.randn(2, 6)
+    dense1.biases += np.random.uniform(
+        -0.05, 0.05, size=dense1.biases.shape
+    ) * np.random.randn(6)
+    dense2.weights += np.random.uniform(
+        -0.05, 0.05, size=dense2.weights.shape
+    ) * np.random.randn(6, 3)
+    dense2.biases += np.random.uniform(
+        -0.05, 0.05, size=dense2.biases.shape
+    ) * np.random.randn(3)
 
-#     # forward pass
-#     dense1.forward(X)
-#     activation1.forward(dense1.output)
-#     dense2.forward(activation1.output)
-#     activation2.forward(dense2.output)
+    # forward pass
+    dense1.forward(X)
+    activation1.forward(dense1.output)
+    dense2.forward(activation1.output)
+    activation2.forward(dense2.output)
 
-#     # calculate loss
-#     loss = loss_function.calculate(activation2.output, y)
+    # calculate loss
+    loss = loss_function.calculate(activation2.output, y)
 
-#     # calculate accuracy
-#     predictions = np.argmax(activation2.output, axis=1)
-#     accuracy = np.mean(predictions == y)
+    # calculate accuracy
+    predictions = np.argmax(activation2.output, axis=1)
+    accuracy = np.mean(predictions == y)
 
-#     # if loss is smaller than the lowest loss, save the weights and biases
-#     if loss < lowest_loss:
-#         print(
-#             f"New set of weights found, iteration {iteration}, loss: {loss:.3f}, accuracy: {accuracy:.3f}"
-#         )
-#         best_dense1_weights = dense1.weights.copy()
-#         best_dense1_biases = dense1.biases.copy()
-#         best_dense2_weights = dense2.weights.copy()
-#         best_dense2_biases = dense2.biases.copy()
-#         lowest_loss = loss
-#     else:
-#         dense1.weights = best_dense1_weights.copy()
-#         dense1.biases = best_dense1_biases.copy()
-#         dense2.weights = best_dense2_weights.copy()
-#         dense2.biases = best_dense2_biases.copy()
+    # if loss is smaller than the lowest loss, save the weights and biases
+    if loss < lowest_loss:
+        print(
+            f"New set of weights found, iteration {iteration}, loss: {loss:.3f}, accuracy: {accuracy:.3f}"
+        )
+        best_dense1_weights = dense1.weights.copy()
+        best_dense1_biases = dense1.biases.copy()
+        best_dense2_weights = dense2.weights.copy()
+        best_dense2_biases = dense2.biases.copy()
+        lowest_loss = loss
+    else:
+        dense1.weights = best_dense1_weights.copy()
+        dense1.biases = best_dense1_biases.copy()
+        dense2.weights = best_dense2_weights.copy()
+        dense2.biases = best_dense2_biases.copy()
