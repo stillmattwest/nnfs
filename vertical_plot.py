5d8f55d05f46f4c6f8df0f07d08fe47bd066c231
stillmattwest <stillmattwest@gmail.com>
2025-04-02 06:05:31 -0700

    adding a visualization for vertical dataset

---
+++ vertical_plot.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import nnfs
+from nnfs.datasets import vertical_data
+
+nnfs.init()
+
+X, y = vertical_data(samples=200, classes=3)
+
+# Plot the dataset
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap="brg", s=40, edgecolors="k")
+plt.title("Vertical Dataset")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.grid(True)
+plt.show()
